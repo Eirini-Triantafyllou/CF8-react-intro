f5dc6f6cf66d93726129323054f34a5942fffd9d
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-10-14 22:01:24 +0300

    feat(form): add MultifieldForm component without validation

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,11 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import NameChanger from "./components/NameChanger.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import Timer from "./components/Timer.tsx";
+// import Layout from "./components/Layout.tsx";
+import RouterLayout from "./components/RouterLayout.tsx";
+import UserPage from "./pages/UserPage.tsx";
+import ControlledInput from "./components/ControlledInput.tsx";
+import UncontrolledInput from "./components/UncontrolledInput.tsx";
 // import WindowSize from "./components/WindowSize.tsx";
 // import NameChangerWithEffect from "./components/NameChangerWithEffect.tsx";
 
@@ -26,6 +31,10 @@ import Timer from "./components/Timer.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
+
+function MultifiedForm() {
+    return null;
+}
 
 function App() {
 
@@ -104,9 +113,39 @@ function App() {
 
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<HomePage />} />
-                    <Route path="name-changer" element={<NameChanger />}/>
-                    <Route path="timer" element={<Timer />}/>
+
+                    <Route element={<RouterLayout/>}>
+
+                     <Route index element={<HomePage />} />
+
+                    {/*<Route path="examples/name-changer" element={<NameChanger />}/>*/}
+                    {/*<Route path="examples/timer" element={<Timer />}/>*/}
+
+
+                      <Route path="examples?">
+                        <Route path="name-changer" element={<NameChanger/>}/>
+                        <Route path="timer" element={<Timer/>}/>
+                         <Route path="controlled-input" element={<ControlledInput/>}/>
+                          <Route path="uncontrolled-input" element={<UncontrolledInput/>}/>
+                          <Route path="multified-form" element={<MultifiedForm/>}/>
+                        {/*<Route path="*" element={<ExamplePage/>}/>*/}
+                      </Route>
+
+                    </Route>
+
+                    {/*<Route path="users/:userId" element={<UserPage />} />*/}
+                    {/*<Route path="users/:userId/accounts" element={<UserAccountPage />} />*/}
+
+                    {/*<Route path="files/*" element={<FilePage />} />*/}
+
+                    <Route path="users">
+                        <Route path=":userId">
+                            <Route index element={<UserPage />}/>
+                            {/*<Route path="accounts" element={<UserAccountPage/>}/>*/}
+                        </Route>
+                    </Route>
+
+                    {/*<Route path="*" element={<PageNotFound/>}/>*/}
                 </Routes>
             </BrowserRouter>
 
